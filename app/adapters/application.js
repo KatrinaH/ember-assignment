ba545cd6e71e158842c3c20dcd37d1616733be25
Katrina Hussain <KatrinaH@users.noreply.github.com>
2015-12-22 21:34:44 -0500

    updates

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-	host: 'http://private-227b9-jsonapifizzbuzz.apiary-mock.com/messages'
+	host: 'http://private-227b9-jsonapifizzbuzz.apiary-mock.com'
 });
 
--- app/models/message.js
+++ app/models/message.js
@@ -1,15 +1,14 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
 	date: DS.attr('date'),
 	useragent: DS.attr('string'),
 	message: DS.attr('string'),
-  //username: DS.attr('string'),
-  pic: DS.attr()
+	username: DS.attr('string'),
+  pic: DS.attr(),
+  fullName: Ember.computed('username', function() {
+  		var str = `${this.get('username')}`;
+			return str.replace(/\./g, ' ').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+		})
 });
-/*
-export default Ember.Object.extend({
-	username: Ember.computed('username', function() {
-		return username.replace(/\./g,' ');
-	});
-});*/
